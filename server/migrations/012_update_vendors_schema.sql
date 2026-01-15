abccd9cb6a12718dafac240d50879adb41a334fb
nandhagopal-05 <nandhagopal050601@gmail.com>
2026-01-15 22:34:59 +0800

    Changes made

--- server/migrations/012_update_vendors_schema.sql
+++ server/migrations/012_update_vendors_schema.sql
@@ -1,22 +1,15 @@
 
 -- Migration to update vendors table structure
+-- CockroachDB does not support DDL inside DO $$ blocks, so we use direct DDL statements.
+
 ALTER TABLE vendors ADD COLUMN IF NOT EXISTS company_name VARCHAR(255);
 ALTER TABLE vendors ADD COLUMN IF NOT EXISTS currency VARCHAR(20);
 ALTER TABLE vendors ADD COLUMN IF NOT EXISTS billing_street TEXT;
 ALTER TABLE vendors ADD COLUMN IF NOT EXISTS billing_address TEXT;
 
--- Rename country to billing_country if it exists as 'country'
-DO $$
-BEGIN
-  IF EXISTS(SELECT * FROM information_schema.columns WHERE table_name='vendors' AND column_name='country') THEN
-    ALTER TABLE vendors RENAME COLUMN country TO billing_country;
-  END IF;
-END $$;
+-- Rename 'country' to 'billing_country'. 
+-- Using IF EXISTS to avoid failure if already renamed.
+ALTER TABLE vendors RENAME COLUMN IF EXISTS country TO billing_country;
 
--- Drop address if it exists (we use billing_street/billing_address now)
-DO $$
-BEGIN
-  IF EXISTS(SELECT * FROM information_schema.columns WHERE table_name='vendors' AND column_name='address') THEN
-    ALTER TABLE vendors DROP COLUMN address;
-  END IF;
-END $$;
+-- Drop 'address' column comfortably
+ALTER TABLE vendors DROP COLUMN IF EXISTS address;
